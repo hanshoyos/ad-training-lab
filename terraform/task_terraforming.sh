c41811148c793f9ff183ba1b19d2bdc6910dc028
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-16 17:34:48 -0400

    Update task_terraforming.sh

--- terraform/task_terraforming.sh
+++ terraform/task_terraforming.sh
@@ -3,4 +3,4 @@
 terraform init
 terraform apply -auto-approve
 terraform output -raw ansible_inventory > ../ansible/inventory/hosts.yml
-echo "[+] run -> ansible-playbook main.yml <- inside the ansible folder!"
+echo "[+] run -> ansible-playbook ad_setup.yml <- inside the ansible folder!"
