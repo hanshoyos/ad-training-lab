cf5c391596854485a1e4206e9580bca4a6d50b97
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-26 15:06:55 -0400

    Update task_terraforming.sh

--- terraform/task_terraforming.sh
+++ terraform/task_terraforming.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-terraform init
-terraform apply -auto-approve
-terraform output -raw ansible_inventory > ../ansible/inventory/hosts.yml
-echo "[+] run -> ansible-playbook ad_setup.yml <- inside the ansible folder!"
+LOGFILE=~/Git_Project/Snare_Lab_POC/setup.log
+
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOGFILE
+}
+
+error_exit() {
+  log "ERROR: $1"
+  exit 1
+}
+
+run_terraform() {
+  echo -e "\n\n####################### Starting Terraform Apply #######################\n" | tee -a $LOGFILE
+
+  log "Initializing Terraform..."
+  terraform init >> $LOGFILE 2>&1 || error_exit "Terraform init failed."
+
+  log "Applying Terraform configuration..."
+  terraform apply -auto-approve >> $LOGFILE 2>&1 || error_exit "Terraform apply failed."
+
+  log "Extracting Ansible inventory..."
+  terraform output -raw ansible_inventory > ../ansible/inventory/hosts.yml || error_exit "Failed to extract Ansible inventory."
+
+  echo -e "\033[1;32m
+  ##############################################################
+  #                                                            #
+  #    Terraform applied successfully.                         #
+  #                                                            #
+  #    Ansible inventory created: ../ansible/inventory/hosts.yml #
+  #                                                            #
+  #    Next step: run the Ansible playbook inside the ansible folder! #
+  #                                                            #
+  #    Command: ansible-playbook ad_setup.yml                  #
+  #                                                            #
+  ##############################################################
+  \033[0m"
+}
+
+run_terraform
